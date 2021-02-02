c5c2b87d1bcbec72392e8a95baa01053dad45a1a
Somik Datta <developersomik@gmail.com>
2021-02-02 23:34:03 +0530

    🐛 FIX: Server Issues

--- client/src/App.js
+++ client/src/App.js
@@ -116,15 +116,49 @@ function App() {
           config: {
     
             iceServers: [
+                // {
+                //     urls: "stun:numb.viagenie.ca",
+                //     username: "sultan1640@gmail.com",
+                //     credential: "98376683"
+                // },
+                // {
+                //     urls: "turn:numb.viagenie.ca",
+                //     username: "sultan1640@gmail.com",
+                //     credential: "98376683"
+                // }
+                {url:'stun:stun01.sipphone.com'},
+                {url:'stun:stun.ekiga.net'},
+                {url:'stun:stun.fwdnet.net'},
+                {url:'stun:stun.ideasip.com'},
+                {url:'stun:stun.iptel.org'},
+                {url:'stun:stun.rixtelecom.se'},
+                {url:'stun:stun.schlund.de'},
+                {url:'stun:stun.l.google.com:19302'},
+                {url:'stun:stun1.l.google.com:19302'},
+                {url:'stun:stun2.l.google.com:19302'},
+                {url:'stun:stun3.l.google.com:19302'},
+                {url:'stun:stun4.l.google.com:19302'},
+                {url:'stun:stunserver.org'},
+                {url:'stun:stun.softjoys.com'},
+                {url:'stun:stun.voiparound.com'},
+                {url:'stun:stun.voipbuster.com'},
+                {url:'stun:stun.voipstunt.com'},
+                {url:'stun:stun.voxgratia.org'},
+                {url:'stun:stun.xten.com'},
                 {
-                    urls: "stun:numb.viagenie.ca",
-                    username: "sultan1640@gmail.com",
-                    credential: "98376683"
+                url: 'turn:numb.viagenie.ca',
+                credential: 'muazkh',
+                username: 'webrtc@live.com'
                 },
                 {
-                    urls: "turn:numb.viagenie.ca",
-                    username: "sultan1640@gmail.com",
-                    credential: "98376683"
+                url: 'turn:192.158.29.39:3478?transport=udp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+                },
+                {
+                url: 'turn:192.158.29.39:3478?transport=tcp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
                 }
             ]
         },
